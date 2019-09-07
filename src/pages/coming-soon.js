39f2d008ef133b0cf368e321e5b0d346793fce26
Ricardo Sawir <ricardosawir@Ricardos-Mackbook-For-now.local>
2019-09-07 18:50:37 +0700

    fixed coming soon

--- src/pages/coming-soon.js
+++ src/pages/coming-soon.js
@@ -5,14 +5,21 @@ import Img from "gatsby-image"
 import { useStaticQuery, graphql } from "gatsby"
 
 const ComingSoonPage = () => {
-  const { backgroundImage } = useStaticQuery(graphql`
+  const { backgroundImage, logo } = useStaticQuery(graphql`
     {
       backgroundImage: file(
         relativePath: { eq: "carousel-hero/citraland-1.jpg" }
       ) {
         childImageSharp {
-          fluid {
+          fluid(quality: 100) {
             ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      logo: file(relativePath: { eq: "ADL-icon.png" }) {
+        childImageSharp {
+          fixed(width: 250) {
+            ...GatsbyImageSharpFixed
           }
         }
       }
@@ -20,30 +27,49 @@ const ComingSoonPage = () => {
   `)
   return (
     <React.Fragment>
-      <SEO title="Oops, Sorry!" />
-      <Layout>
-        <div className="container my-5 text-center">
-          <div className="position-relative w-100">
-            <Img
-              className="position-absolute w-100"
-              style={{ top: "0", left: "0", height: "100%" }}
-              fluid={backgroundImage.childImageSharp.fluid}
-            />
-            <div className="position-absolute">
-              <h1 className="text-center">We're rebuilding our website</h1>
-              <p className="lead">
-                For inquiries or question, email{" "}
-                <a
-                  href="mailto:lukito@adlpartnership.com"
-                  className="text-dark text-decoration-none"
-                >
-                  Lukito@adlpartnership.com
-                </a>
-              </p>
-            </div>
-          </div>
+      <SEO title="Coming Soon!" />
+      <div className="position-relative w-100" style={{ height: "100vh" }}>
+        <Img
+          className="position-absolute w-100"
+          style={{ top: "0", left: "0", height: "100%" }}
+          fluid={backgroundImage.childImageSharp.fluid}
+        />
+        <div
+          className="position-absolute w-100"
+          style={{
+            top: "0",
+            left: "0",
+            right: "0",
+            bottom: "0",
+            height: "100%",
+            background: "rgba(0,0,0,0.35)",
+          }}
+        />
+        <div
+          className="position-absolute d-flex flex-column justify-content-center text-light"
+          style={{
+            top: "0",
+            left: "0",
+            right: "0",
+            bottom: "0",
+            height: "100%",
+          }}
+        >
+          <Img className="mx-auto" fixed={logo.childImageSharp.fixed} />
+          <h1 className="display-4 text-center">
+            We're rebuilding our website
+          </h1>
+          <p className="lead text-center">
+            For inquiries or questions,{" "}
+            <a
+              href="mailto:lukito@adlpartnership.com"
+              className="text-light text-decoration-none"
+            >
+              click here or email lukito@adlpartnership.com
+            </a>
+          </p>
         </div>
-      </Layout>
+      </div>
     </React.Fragment>
   )
 }
