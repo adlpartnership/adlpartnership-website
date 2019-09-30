838e89ec48065021c5fa4ad841633fdd42ae2be6
Ricardo Sawir <ricardosawir@Ricardos-Mackbook-For-now.local>
2019-09-30 12:03:11 +0700

    updated home component

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,15 +4,40 @@ import CarouselHero from "../components/carouselHero"
 import { graphql, navigate } from "gatsby"
 import SEO from "../components/seo"
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
   return (
     <React.Fragment>
       <SEO title="Welcome" />
       <Layout>
-        <CarouselHero />
+        <CarouselHero data={data.featured.edges} />
       </Layout>
     </React.Fragment>
   )
 }
 
 export default IndexPage
+
+export const query = graphql`
+  query MyQuery {
+    featured: allWordpressWpPortfolio(
+      filter: { acf: { include_in_home_page: { eq: true } } }
+    ) {
+      edges {
+        node {
+          id
+          title
+          slug
+          featured_media {
+            localFile {
+              childImageSharp {
+                fluid(quality: 100) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
