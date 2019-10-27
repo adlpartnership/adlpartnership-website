2a8c7e27d27f27d74133d16d39e060a6f4fa4417
Ricardo Sawir <ricardosawir@Ricardos-Mackbook-For-now.local>
2019-10-27 15:41:59 +0700

    updated about us

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -48,32 +48,6 @@ const AboutUsPage = () => {
                 design process should be the collaboration among all
                 stakeholders to accommodate different needs.
               </p>
-              <p>SERVICE and EXPERTISE</p>
-              <p>
-                ABOUT US ADL (Architecture Design Laboratory) was founded in
-                Singapore in 2015 by a group of architects, who are passionate
-                about architecture and urban planning to form a unique design
-                firm. The firm brings in collective international and local
-                experience of the global - local team with strong design
-                research culture to deliver quality design solution. Based in
-                Singapore, Jakarta and Surabaya - Indonesia, ADL comprises of
-                architectural design and planning professionals who have worked
-                in renowned international design firms with experience across
-                regions, bringing a unique blend of global exposure yet fully
-                attuned to the local culture. The partners believe that good
-                design should be simple, easy to comprehend. It should create
-                spaces that are comfortable, practical yet leaving deep
-                impressions for the users by balancing functional requirements
-                with design creativity. The design process should be the
-                collaboration among all stakeholders to accommodate different
-                needs.
-              </p>
-              <p>
-                The firm’s key sectors expertise are in urban developments,
-                particularly in mixed-use, retail, residential, office,
-                hospitality, healthcare, master planning and transit-oriented
-                developments.
-              </p>
             </div>
           </div>
         </div>
